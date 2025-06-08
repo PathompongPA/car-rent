ac980a515f32bb28f44dfc61a9da2f5858ddf88e
l.pathompong <pathompong.litk@gmail.com>
2025-06-09 05:17:36 +0700

    style(navbar) : update and fix style navbar component

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -3,35 +3,24 @@ import { mockup } from "../mockup";
 
 export default function NavigationBar() {
     const { component } = mockup;
-
-    function BtnHamburgerMenu({ className }) {
-        function handleBtnHamburgerMenu() {
-            document.getElementsByClassName("navigation__menu")[0].classList.toggle("hidden")
-        }
-        return (
-            <a onClick={handleBtnHamburgerMenu} className={className} >
-                <div className={`navigation__element-hamburger-menu >> bg-blue-2 w-[40px] h-[7px] rounded-lg`} ></div>
-                <div className={`navigation__element-hamburger-menu >> bg-blue-2 w-[40px] h-[7px] rounded-lg`} ></div>
-                <div className={`navigation__element-hamburger-menu >> bg-blue-2 w-[40px] h-[7px] rounded-lg`} ></div>
-            </a>
-        )
-    }
-
-    function Title({ TitleList, className }) {
-        return TitleList.map(({ text, link }, index) => {
-            return <Link className={className} key={text + index} href={link} > {text} </Link>
-        })
-    }
+    const { navbar } = component;
+    let handleBtnHamburgerMenu = () => setTimeout(() => { document.getElementsByClassName("navigation__menu")[0].classList.toggle("hidden") }, 75);
 
     return (
-        <div className="navigation >> flex justify-center fixed | top-0 z-10 w-full | md:sticky | md:border-b-4 md:border-golden-1 md:bg-white md:drop-shadow-xl/25">
-            <div className="navigation__container >> flex flex-col grow bg-transparent | max-w-7xl  max-h-[80px] | md:items-center md:flex-row">
-                <div className="navigation__container-logo >> flex grow justify-between items-center | py-2 pr-2 | bg-white | border-b-4 border-golden-1 | md:border-b-0 ">
+        <div className="navigation >> flex justify-center fixed | top-0 z-10 w-full |  | md:border-b-4 md:border-golden-1 md:bg-white md:drop-shadow-xl/25">
+            <div className="navigation__container >> flex flex-col grow bg-transparent | max-w-7xl  md:h-[80px] | md:items-center md:flex-row">
+                <div className="navigation__container-logo >> flex grow justify-between items-center | py-2 pr-4 | bg-white | border-b-4 border-golden-1 | md:border-b-0 ">
                     <img className="navigation__logo >> aspect-16/9 object-cover | max-h-[60px]" src={component.navbar.logo} alt="logo" />
-                    <BtnHamburgerMenu className={"navigation__btn-hamburger-menu >> flex flex-col items-end | gap-[5px] | md:hidden"} />
+                    <a className="navigation__btn-hamburger-menu --btn >> flex flex-col items-end | gap-[5px] | md:hidden" onClick={handleBtnHamburgerMenu} >
+                        <div className={`navigation__element-hamburger-menu >> bg-blue-1 w-[40px] h-[7px] rounded-lg `} ></div>
+                        <div className={`navigation__element-hamburger-menu >> bg-blue-1 w-[40px] h-[7px] rounded-lg `} ></div>
+                        <div className={`navigation__element-hamburger-menu >> bg-blue-1 w-[40px] h-[7px] rounded-lg `} ></div>
+                    </a>
                 </div>
-                <div className="navigation__menu >> hidden flex flex-col items-end | p-4 gap-6 grow | bg-blue-1/5 backdrop-blur-lg | md:flex md:flex-row md:justify-end | md:gap-8 | md:bg-transparent">
-                    <Title className={"navigation__title >> --btn >> w-fit | text-blue-1 font-extrabold "} TitleList={component.navbar.title.list} />
+                <div className="navigation__menu >> hidden flex flex-col items-end | h-[100vh] p-4 gap-6 grow | bg-white/10 backdrop-blur-lg | md:flex md:flex-row md:justify-end | md:gap-8 md:h-fit | md:bg-transparent " onClick={handleBtnHamburgerMenu}>
+                    {navbar.title.list.map(({ text, link }, index) =>
+                        <Link className="navigation__title --btn >> w-full text-end | text-blue-1 font-extrabold | md:w-fit | active:text-golden-1" key={text + index} href={link} > {text} </Link>
+                    )}
                 </div>
             </div>
         </div>

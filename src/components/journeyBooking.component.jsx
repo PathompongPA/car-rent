7989f190d9421e15cc4b5429d151c383ac72711a
l.pathompong <pathompong.litk@gmail.com>
2025-06-10 10:31:31 +0700

    change(journeyBooking) : change style reponsive sm > lg

--- src/components/journeyBooking.component.jsx
+++ src/components/journeyBooking.component.jsx
@@ -11,13 +11,13 @@ export default function JourneyBooking() {
     }
 
     return (
-        <div className=" journey-booking >> flex flex-col justify-center items-center | p-4 py-8 gap-8 w-full h-fit | md:h-[700px] md:max-w-7xl md:py-16 " >
+        <div className=" journey-booking >> flex flex-col justify-center items-center | p-4 py-8 gap-8 w-full h-fit | md:max-w-7xl md:py-16 " >
             <h1 className="journey-booking__title >> drop-shadow-2xl/50 text-blue-1 font-black text-[42px] | md:text-[64px]">{journeyBooking.title}</h1>
-            <div className="journey-booking__container-card >> flex justify-center items-center flex-wrap | gap-4 w-full h-fit | md:flex-nowrap | md:h-[500px] md:gap-8">
+            <div className="journey-booking__container-card >> flex justify-center items-center flex-wrap | gap-4 w-full h-fit | md:gap-8 border">
                 {journeyBooking.card.list.map(({ title, detail }, index) =>
-                    <div className={`journey-booking__card --btn >> flex flex-col justify-center items-center | w-full min-h-[180px] | bg-blue-2 | drop-shadow-xl/50 rounded-lg | text-white text-xl font-extrabold | md:w-[400px] text-center md:min-h-[400] md:h-full`} onClick={handleHoverCard} key={`journey-booking-card-${index}`} index={index}>
-                        <h1 className="journey-booking__card-title >> flex justify-center items-center | p-8 | md:w-full md:h-[150px] | md:text-4xl" index={index}> {title} </h1>
-                        <div className={` jouryney-booking__container-detail >> flex flex-col justify-center| w-full p-4 gap-4 | bg-white | rounded-lg | md:items-start md:justify-start | md:min-h-[300px] md:max-w-full | ${locatoion === index ? "" : "md:hidden"} `} index={index}>
+                    <div className={`journey-booking__card --btn >> flex flex-col justify-center items-center | w-full min-h-[180px] | bg-blue-2 | drop-shadow-xl/50 rounded-lg | text-white text-xl font-extrabold | lg:w-[390px] text-center lg:h-[450px] `} onClick={handleHoverCard} key={`journey-booking-card-${index}`} index={index}>
+                        <h1 className="journey-booking__card-title >> flex justify-center items-center | p-8 | md:w-full lg:h-[150px] | lg:text-4xl" index={index}> {title} </h1>
+                        <div className={` jouryney-booking__container-detail >> flex flex-col justify-center| w-full p-4 gap-4 | bg-white | rounded-lg | md:items-start md:justify-start | md:min-h-[300px] md:max-w-full | ${locatoion === index ? "" : "lg:hidden"} `} index={index}>
                             {detail.map((param, _index) =>
                                 <div className={`journey-booking__card-detail >> flex justify-start items-start | h-fit | text-base font-bold text-blue-1`} key={`journey-booking-card-detail-${_index}`} index={_index}>{`- ${param}`}</div>
                             )}
